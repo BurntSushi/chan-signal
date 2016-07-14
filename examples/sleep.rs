a5e0567bd28bb06c03282c6259af37c22c4aca0d
Mickaël Salaün <mic@digikod.net>
2016-07-14 13:12:31 +0200

    examples: Fix typo

--- examples/sleep.rs
+++ examples/sleep.rs
@@ -8,7 +8,7 @@ use chan_signal::{Signal, notify};
 
 fn main() {
     let signal = notify(&[Signal::INT]);
-    println!("Send a TERM signal my way!");
+    println!("Send a INT signal my way!");
     thread::spawn(move || thread::sleep(Duration::from_secs(10)));
     // block until we get a signal
     assert_eq!(signal.recv(), Some(Signal::INT));
