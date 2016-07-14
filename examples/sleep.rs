ebd6dae4afbcf948a8e733dc007365bc68037804
Mickaël Salaün <mic@digikod.net>
2016-07-14 13:07:33 +0200

    examples: Replace deprecated sleep_ms() with sleep()
    std::thread::sleep_ms() is deprecated since Rust 1.6.0

--- examples/sleep.rs
+++ examples/sleep.rs
@@ -2,14 +2,14 @@
 extern crate chan_signal;
 
 use std::thread;
+use std::time::Duration;
 
 use chan_signal::{Signal, notify};
 
 fn main() {
     let signal = notify(&[Signal::INT]);
     println!("Send a TERM signal my way!");
-    thread::spawn(move || thread::sleep_ms(10000));
-    // thread::sleep_ms(5000);
+    thread::spawn(move || thread::sleep(Duration::from_secs(10)));
     // block until we get a signal
     assert_eq!(signal.recv(), Some(Signal::INT));
     println!("Thanks :]");
